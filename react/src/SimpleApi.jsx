b4ab2820b7dfc133e06e9d72d69b73650df4d72a
piyush <piyush.khadka2233@gmail.com>
2026-01-15 16:21:32 +0000

    data fetched and displayed

--- react/src/SimpleApi.jsx
+++ react/src/SimpleApi.jsx
@@ -1,38 +1,68 @@
 import { useState, useEffect } from "react";
 const SimpleApi = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  const [characters, setCharacters] = useState([]);
+  const [charInfo, setCharInfo] = useState(null);
 
   useEffect(() => {
     const getData = async () => {
       try {
+        setError(null);
         setLoading(true);
-        const respone = await fetch(
-          `https://rickandmortyapi.com/api/character/1`
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/character`
         );
-        const data = await respone.json();
-        setData(data);
+        const data = await response.json();
+        setCharacters(data.results);
       } catch (error) {
-        console.error("Error Fetching Data: ", error);
+        console.error("Error fetching data", error);
         setError(error.message);
+        setLoading(false);
       } finally {
         setLoading(false);
       }
     };
     getData();
   }, []);
+  const handleInfo = (info) => {
+    setCharInfo(info);
+  };
+  console.log(charInfo);
+
   return (
     <div>
       {loading && <div>loading...</div>}
       {error && <div> error : {error}</div>}
-      {!loading && !error && data && (
+      {!loading && !error && characters && (
         <div>
-          <h1>{data.name}</h1>
-          <img src={data.image} alt={data.name} />
-          <p>Status: {data.status}</p>
-          <p>Species: {data.species}</p>
-          <p>Gender: {data.gender}</p>
+          {characters.map((char) => {
+            return (
+              <div key={char.id}>
+                <img src={char.image} alt={char.name} />
+                <h3>{char.name}</h3>
+                <p>
+                  {char.status} - {char.species}
+                </p>
+                <button onClick={() => handleInfo(char)}>More Info</button>
+              </div>
+            );
+          })}
+        </div>
+      )}
+
+      {charInfo && (
+        <div>
+          <div>
+            <button onClick={() => setCharInfo(null)}>Close</button>
+            <h1>{charInfo.name}</h1>
+            <img src={charInfo.image} alt={charInfo.name} />
+            <p>Status: {charInfo.status}</p>
+            <p>Species: {charInfo.species}</p>
+            <p>Gender: {charInfo.gender}</p>
+            <p>Origin: {charInfo.origin.name}</p>
+            <p>Location: {charInfo.location.name}</p>
+          </div>
         </div>
       )}
     </div>
