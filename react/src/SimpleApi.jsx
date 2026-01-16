c391c5eaf9c3d9f26f20751c30403bcdb134d6c7
piyush <piyush.khadka2233@gmail.com>
2026-01-16 17:46:56 +0000

    search using fetch

--- react/src/SimpleApi.jsx
+++ react/src/SimpleApi.jsx
@@ -4,36 +4,46 @@ const SimpleApi = () => {
   const [error, setError] = useState(null);
   const [characters, setCharacters] = useState([]);
   const [charInfo, setCharInfo] = useState(null);
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        setError(null);
-        setLoading(true);
-        const response = await fetch(
-          `https://rickandmortyapi.com/api/character`
-        );
-        const data = await response.json();
-        setCharacters(data.results);
-      } catch (error) {
-        console.error("Error fetching data", error);
-        setError(error.message);
-        setLoading(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
-  }, []);
-  const handleInfo = (info) => {
-    setCharInfo(info);
+  const fetchapi = async (url) => {
+    try {
+      setError(null);
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      setCharacters(data.results);
+    } catch (error) {
+      console.error("Error fetching data", error);
+      setError(error.message);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
   };
-  console.log(charInfo);
+  useEffect(() => {
+    fetchapi("https://rickandmortyapi.com/api/character");
+  }, []);
+
+  const handleInfo = (char) => {
+    setCharInfo(char);
+  };
+  const handleSearch = async () => {
+    if (search === "") return;
+    fetchapi(`https://rickandmortyapi.com/api/character/?name=${search}`);
+  };
 
   return (
     <div>
       {loading && <div>loading...</div>}
       {error && <div> error : {error}</div>}
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+      ></input>
+      <button onClick={handleSearch}>search</button>
       {!loading && !error && characters && (
         <div>
           {characters.map((char) => {
