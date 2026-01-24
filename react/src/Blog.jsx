bc94f3e8212b3e08edb4b2715c8aeb00eaf8b576
piyush <piyush.khadka2233@gmail.com>
2026-01-24 15:40:39 +0000

    json post request

--- react/src/Blog.jsx
+++ react/src/Blog.jsx
@@ -1,17 +1,35 @@
+import { useState, useEffect } from "react";
 const Blog = () => {
   //http://localhost:3000/data
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = async () => {
+    const response = await fetch("http://localhost:3000/data", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, author, body }),
+    });
+    if (!response.ok) throw Error("Server error");
+  };
+
   return (
-    <dvi>
+    <div>
       <div>
         <p>Write your own blog</p>
-        <p>Title</p> <input></input>
+        <p>Title</p>{" "}
+        <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
         <p>Author</p>
-        <input></input>
+        <input value={author} onChange={(e) => setAuthor(e.target.value)} />
         <p>Body</p>
-        <textarea></textarea>
-        <button>submit</button>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <button onClick={handleSubmit}>submit</button>
       </div>
-    </dvi>
+    </div>
   );
 };
 
